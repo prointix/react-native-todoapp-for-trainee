a12fda12b2fde1cf333c3ba6eb8120505819bfd4
Sothirich <peangsereysothirich@gmail.com>
2022-08-18 11:57:40 +0700

    Todo index

--- app/services/todo.js
+++ app/services/todo.js
@@ -1,8 +1,34 @@
 // TODO: Add a todo service that provides CRUD operations for todos.
 
-export const getTodos = () => {
-  return [
-    {id: 1, text: 'Learn React', completed: false},
-    {id: 2, text: 'Learn Redux', completed: false},
-  ];
+import {useCallback, useEffect, useState} from 'react';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const useTodos = p => {
+  const [page, setPage] = useState(p);
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getAllTodos = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/todos?_limit=20&_page=${page}`);
+      const data = await res.json();
+
+      setTodos(prev => prev.concat(data));
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+      setIsLoading(false);
+    }
+    setIsLoading(false);
+  }, [page]);
+
+  useEffect(() => {
+    getAllTodos();
+  }, [getAllTodos]);
+
+  return {todos, error, isLoading, page, setPage};
 };
