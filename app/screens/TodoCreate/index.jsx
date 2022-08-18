9c94b69ba763bd353a07ef3ef12b45144f873679
Sothirich <peangsereysothirich@gmail.com>
2022-08-18 11:56:41 +0700

    create todo page

--- app/screens/TodoCreate/index.jsx
+++ app/screens/TodoCreate/index.jsx
@@ -1,21 +1,117 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Fontisto';
+import LinearGradient from 'react-native-linear-gradient';
 
-const TodoCreate = () => {
+const TodoCreate = ({navigation}) => {
+  const onBackPressHandler = () => {
+    navigation.goBack();
+  };
+
+  const onAddTodoPressHandler = () => {};
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Todo Create</Text>
+    <LinearGradient
+      colors={['#d1d3ff', '#9A9CCD']}
+      start={{x: 0.0, y: 0.0}}
+      end={{x: 1.0, y: 1.0}}
+      style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>ADD TODO</Text>
       </View>
-    </SafeAreaView>
+      <TouchableOpacity style={styles.icon} onPress={onBackPressHandler}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+      </TouchableOpacity>
+
+      <View style={styles.body}>
+        <TextInput placeholder="TODO Title" style={styles.textInput} />
+        <TextInput placeholder="TODO SubTitle" style={styles.textInput} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color: '#9A9CCE', fontSize: 20, fontWeight: 'bold'}}>
+            Add Todo
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#C2C3F4',
+  },
+  header: {
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#9395D3',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#fff',
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bigText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'black',
+  },
+  smallText: {
+    fontWeight: 'bold',
+    fontSize: 12,
+    color: 'black',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    top: 8,
+    left: 5,
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'black',
+  },
+  textInputContainer: {
+    padding: 5,
+    flexDirection: 'row',
+    borderRadius: 15,
+    marginVertical: 9,
+    width: 350,
+    elevation: 3,
+  },
+  textInput: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2,
+    width: '90%',
+    justifyContent: 'center',
+    alignContent: 'center',
+    fontSize: 20,
+    marginVertical: 15,
+  },
+  button: {
+    marginVertical: 45,
+    width: '50%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderRadius: 30,
+    padding: 10,
+    shadowOpacity: 1,
+    elevation: 5,
   },
 });
 
