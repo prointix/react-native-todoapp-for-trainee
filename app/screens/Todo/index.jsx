06e8c8a33ac2559cfc4bd118711c83f5439f439d
Sothirich <peangsereysothirich@gmail.com>
2022-08-18 11:57:50 +0700

    Todo index

--- app/screens/Todo/index.jsx
+++ app/screens/Todo/index.jsx
@@ -1,22 +1,168 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View, FlatList} from 'react-native';
+import Icon from 'react-native-vector-icons/Fontisto';
+import LinearGradient from 'react-native-linear-gradient';
+import {useTodos} from '../../services/todo';
 
-const Todo = () => {
+const Todo = ({navigation}) => {
+  const {todos, setPage} = useTodos(1);
+  const [tab, setTab] = useState('TODOs'); // todos or done
+  const doneTodo = todos.filter(status => status.completed);
+  const notDoneTodo = todos.filter(status => !status.completed);
+
+  const onDonePressHandler = () => {
+    setTab('DONE');
+  };
+
+  const onTodoPressHandler = () => {
+    setTab('TODOs');
+  };
+
+  const onEndReachedHandler = () => {
+    const page = todos.length / 20 + 1;
+    console.log(page);
+    setPage(page);
+    useTodos;
+  };
+
+  const ListItem = ({todo}) => {
+    return (
+      <View style={styles.listItem}>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={styles.bigText}>ID: {todo.id}</Text>
+          <Text style={styles.smallText}>{todo.title}</Text>
+        </View>
+        <TouchableOpacity style={[styles.actionIcon]}>
+          {todo.completed ? (
+            <Icon name={'checkbox-active'} solid size={20} color="black" />
+          ) : (
+            <Icon name={'checkbox-passive'} solid size={20} color="black" />
+          )}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const onAddPressHandler = () => {
+    navigation.navigate('TodoCreate');
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View></View>
-    </SafeAreaView>
+    <LinearGradient
+      colors={['#d1d3ff', '#9A9CCD']}
+      start={{x: 0.0, y: 0.0}}
+      end={{x: 1.0, y: 1.0}}
+      style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => onTodoPressHandler()}>
+          <Text
+            style={tab === 'TODOs' ? styles.focusedText : styles.unFocusedText}>
+            TODO
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.bigText}>|</Text>
+        <TouchableOpacity onPress={() => onDonePressHandler()}>
+          <Text
+            style={tab === 'DONE' ? styles.focusedText : styles.unFocusedText}>
+            DONE
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {tab === 'DONE' ? (
+        <View style={styles.body}>
+          <FlatList
+            data={doneTodo}
+            renderItem={({item}) => <ListItem todo={item} />}
+            onEndReached={onEndReachedHandler}
+            ListFooterComponent={() => <Text>Loading....</Text>}
+          />
+        </View>
+      ) : (
+        <View style={styles.body}>
+          <FlatList
+            data={notDoneTodo}
+            renderItem={({item}) => <ListItem todo={item} />}
+            onEndReached={onEndReachedHandler}
+            ListFooterComponent={() => <Text>Loading....</Text>}
+          />
+        </View>
+      )}
+      <TouchableOpacity style={styles.button} onPress={onAddPressHandler}>
+        <Icon name="plus-a" size={20} />
+      </TouchableOpacity>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#9395D3',
+  },
+  bigText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'black',
+  },
+  smallText: {
+    fontWeight: 'bold',
+    fontSize: 12,
+    color: 'black',
   },
   header: {
+    padding: 15,
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#9395D3',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  focusedText: {
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    fontSize: 30,
+    color: '#fff',
+  },
+  unFocusedText: {
+    fontSize: 30,
+    color: '#fff',
+  },
+  body: {
+    flex: 1,
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    bottom: 20,
+    right: 10,
+    backgroundColor: '#B3B7EE',
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+  },
+  actionIcon: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    borderRadius: 3,
+    alignSelf: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    borderRadius: 7,
+    marginVertical: 9,
+    width: 350,
+    elevation: 3,
   },
 });
 
